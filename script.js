e9810dbac142cda9888cf25108b805f632cd3cce
IDCSA07 <cdelacruzs2201@alumno.ipn.mx>
2024-09-24 17:54:16 -0600

    hola

---
+++ script.js
@@ -0,0 +1,26 @@
+//clase 24/09/24
+//nombre: De la cruz Sanchez Cesar Ignacio 
+//VARIABLES 
+/*LAS VARIABLES GUARDAN INFORMACION QUE PUEDES REUTILIZAR*/
+
+//DECLARACION DE VARIABLES 
+
+let nombre = 'Pedro';
+const PI = '3.1416';
+var edad = '25';
+
+//operadores 
+
+let a = 5;
+let b = 2;
+let suma = a + b;
+let resta = a - b;
+let multiplicacion = a * b;
+let division = a / b;
+
+//operadores de comparacion
+
+a > b;
+a < b;
+a == b;
+a === b;
