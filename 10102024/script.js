b80929a1e9334d3183c354e29c73185c87ee0262
.\index.htmIDCSA07 <cdelacruzs2201@alumno.ipn.mx>
2024-10-10 16:46:57 -0600

    subir otra carpeta

---
+++ 10102024/script.js
@@ -0,0 +1,9 @@
+//comentario de una linea
+/*comentadio multilinea */
+function hello(){
+    return 'hola js'
+
+}
+
+console.log(hello());
+//Investigar funciones en javascript diferentes usos de funciones en js y ejemplificar cada una de ellas 
